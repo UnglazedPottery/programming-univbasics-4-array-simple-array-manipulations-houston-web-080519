7636e082ac17cf81c49055eda88b98fc262fbdbf
Jade Huynh <35jadeh@gmail.com>
2019-07-19 21:29:57 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push(Array, String)
+  Array.push(String)
+end
+
+def using_unshift(Array, String)
+  Array.unshift(String)
+end
+
+def using_pop(Array)
+  Array.pop()
+end
+
+def pop_with_args(Array)
+  Array.pop(2)
+end
+
+def using_shift(Array)
+  Array.shift()
+end
+
+def shift_with_args(Array)
+  Array.shift(2)
+end
+
+def using_concat(Array, Array2)
+  Array.concat(Array2)
+end
+
+def using_insert(Array, element)
+  Array[4].insert(element)
+end
+
+def using_uniq(Array)
+  Array.uniq()
+end
+
+def using_flatten(Array)
+  Array.flatten()
+end
+
+def using_delete(Array, String)
+  Array.delete(String)
+end
+
+def using_delete_at(Array, Integer)
+  Array[Integer].delete()
+end
+
